df4e6c00eeeb0b66bb596ce68e6c80a298260160
jmhutfles <jmhutfles@gmail.com>
2025-04-15 16:27:45 -0400

    Made it able to read FS and FS 2.0 data

--- read flysight.py
+++ read flysight.py
@@ -22,9 +22,25 @@ PathJumperRaw = filedialog.askopenfilename(title="Select Jumper Data")
 # PathWindPack = Path(r'C:\Users\jhutfles\Desktop\Glide Sensor\Glide Analysis\March 2025 Test Session\L2P1\FS 11.CSV')
 # PathJumperRaw = Path(r'C:\Users\jhutfles\Desktop\Glide Sensor\Glide Analysis\March 2025 Test Session\L2P1\FS 10.CSV')
 
-DataHeaders = ["Idk What this means", "Time", "Latitude", "Longitude", "Altitude MSL", "North Velocity", "East Velocity", "Down Velocity", "hAcc", "vAcc", "sAcc", "numSV"]
-WindPack = pd.read_csv(PathWindPack, names=DataHeaders, skiprows = 7)
-JumperRaw = pd.read_csv(PathJumperRaw, names=DataHeaders, skiprows = 7)
+DataHeaders = ["Time", "Latitude", "Longitude", "Altitude MSL", "North Velocity", "East Velocity", "Down Velocity", "hAcc", "vAcc", "sAcc", "heading", "cAcc", "gpsFix", "numSV"]
+DataHeaders2 = ["Time", "Latitude", "Longitude", "Altitude MSL", "North Velocity", "East Velocity", "Down Velocity", "hAcc", "vAcc", "sAcc", "numSV"]
+WindPack = pd.read_csv(PathWindPack,skiprows = 7)
+JumperRaw = pd.read_csv(PathJumperRaw, skiprows = 7)
+
+#Accound for extra first colloumn in new Flysights & Naming Columns
+if WindPack.iloc[0,0] == "$GNSS":
+    WindPack.drop(WindPack.columns[0], axis = 1, inplace = True)
+    WindPack.columns = DataHeaders2
+else:
+    WindPack.columns = DataHeaders
+
+
+if JumperRaw.iloc[0,0] == "$GNSS":
+    JumperRaw.drop(JumperRaw.columns[0], axis = 1, inplace = True)
+    JumperRaw.columns = DataHeaders2
+else:
+    JumperRaw.columns = DataHeaders
+
 
 
 
@@ -49,13 +65,11 @@ filteredJumperRaw = JumperRaw[mask]
 
 
 #Creating a function that describes wind as a function of altitude
-# Eastcoeff = np.polyfit(filteredWindPack["Altitude MSL"], filteredWindPack["East Velocity"], deg=10)
-# East = np.poly1d(Eastcoeff)
-# Northcoeff = np.polyfit(filteredWindPack["Altitude MSL"], filteredWindPack["North Velocity"], deg=10)
-# North = np.poly1d(Northcoeff)
+
 #Clean Up data to allow spline fit to work
 filteredWindPack = filteredWindPack.sort_values("Altitude MSL")
 filteredWindPack = filteredWindPack.dropna(subset=["Altitude MSL", "East Velocity", "North Velocity"])
+
 East = UnivariateSpline(
     filteredWindPack["Altitude MSL"],
     filteredWindPack["East Velocity"],
@@ -115,7 +129,7 @@ GlideAGLft = GlideAGLft.rename(columns={"Altitude MSL": "Altitude AGL (ft)"})
 #Plot the two datasets
 plt.figure()
 plt.scatter(filteredWindPack["Altitude MSL"], filteredWindPack["North Velocity"], label="North Wind", color = "r")
-plt.plot(filteredWindPack["Altitude MSL"], North(filteredWindPack["Altitude MSL"]), label = "Polynomial Fit", color = "g")
+plt.plot(filteredWindPack["Altitude MSL"], North(filteredWindPack["Altitude MSL"]), label = "Spline Fit", color = "g")
 plt.xlabel("Altitude MSL (m)")
 plt.ylabel("Velocity (m/s)")
 plt.title("North Wind Velocity form WindPack")
@@ -123,7 +137,7 @@ plt.legend()
 
 plt.figure()
 plt.scatter(filteredWindPack["Altitude MSL"], filteredWindPack["East Velocity"], label="East Wind", color = "r")
-plt.plot(filteredWindPack["Altitude MSL"], East(filteredWindPack["Altitude MSL"]), label = "Polynomial Fit", color = "g")
+plt.plot(filteredWindPack["Altitude MSL"], East(filteredWindPack["Altitude MSL"]), label = "Spline Fit", color = "g")
 plt.xlabel("Altitude MSL (m)")
 plt.ylabel("Velocity (m/s)")
 plt.title("East Wind Velocity form WindPack")
