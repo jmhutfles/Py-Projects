abe2041a4bf5db7b39695b0564fc0ef2c28e2ef6
jmhutfles <jmhutfles@gmail.com>
2025-04-23 14:24:55 -0400

    Updating UI and adding Picture

--- Launch_UI.py
+++ Launch_UI.py
@@ -1,14 +1,27 @@
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
+from PIL import Image, ImageTk
+import os
 
 def RunTool(Tool):
   subprocess.run(["Python", Tool])
 
+#Configure Window
 root = tk.Tk()
-root.state("zoomed")
 root.configure(bg='black')
 root.title("Choose a Tool")
+#Add Background Image
+BackgroundImagePath = os.path.join("Pictures", "Test Session Pictures.jpg")
+BackgroundImage = Image.open(BackgroundImagePath)
+Background = ImageTk.PhotoImage(BackgroundImage)
+bg_label = tk.Label(root, image=Background)
+bg_label.place(relwidth=1, relheight=1)
+width, height = BackgroundImage.size
+root.geometry(f"{width}x{height}")
+
+
+
 
 tk.Button(root, 
           text="FlySight Wind Correction", 
