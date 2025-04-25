879b1b6122c2d09dd7cd79794a7ce3dbc4b73d11
jmhutfles <jmhutfles@gmail.com>
2025-04-25 08:13:22 -0400

    Graph of raw

--- ABT_Quick_View.py
+++ ABT_Quick_View.py
@@ -12,8 +12,15 @@ import Conversions
 import time
 
 #Get Data
+root = tk.Tk()
+root.withdraw()
 Data = ReadRawData.ReadABT("Select the ABT file.")
 
 plt.figure()
 plt.scatter(Data["Time"], Data["Ax"], label="Ax", color="g")
+plt.scatter(Data["Time"], Data["Ay"], label="Ay", color="r")
+plt.scatter(Data["Time"], Data["Az"], label="Az", color="b")
+plt.scatter(Data["Time"], Data["P"], label="Pressure", color="black")
+plt.scatter(Data["Time"], Data["T"], label="Temperature", color="y")
+plt.legend()
 plt.show()
