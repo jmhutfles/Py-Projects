1db262d7fec6d2f59cc97c4d3329272a379c0ff9
jmhutfles <jmhutfles@gmail.com>
2025-06-19 16:55:38 -0400

    Put in loops to not break program

--- Launch_UI.py
+++ Launch_UI.py
@@ -1,20 +1,26 @@
+import sys
+import os
 import tkinter as tk
 from tkinter import messagebox
 import subprocess
 from PIL import Image, ImageTk
-import os
 from UIFunctions import RunTool
 from UIFunctions import ConversionsWindow
 
 
 
-#Configure Window
+# Configure Window
 root = tk.Tk()
 root.configure(bg='black')
 root.title("Choose a Tool")
-#Add Background Image
-script_dir = os.path.dirname(os.path.abspath(__file__))
-BackgroundImagePath = os.path.join(script_dir, "Pictures", "Test Session Pictures.jpg")
+
+# Add Background Image
+if getattr(sys, 'frozen', False):
+    base_dir = sys._MEIPASS
+else:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+BackgroundImagePath = os.path.join(base_dir, "Pictures", "Test Session Pictures.jpg")
 BackgroundImage = Image.open(BackgroundImagePath)
 Background = ImageTk.PhotoImage(BackgroundImage)
 bg_label = tk.Label(root, image=Background)
@@ -38,7 +44,7 @@ for text, script in button_specs:
     tk.Button(
         root,
         text=text,
-        command=lambda s=script: RunTool(s, script_dir, root),
+        command=lambda s=script: RunTool(s, base_dir, root),
         height=2,
         width=23
     ).pack(pady=15)
